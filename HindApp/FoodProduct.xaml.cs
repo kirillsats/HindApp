1fe393811420cfeb59bc0ae3f2b0e869a5357526
opilane <opilane@HP-CZC2349HT9>
2025-04-30 13:12:29 +0300

    Add project files.

---
+++ HindApp/FoodProduct.xaml.cs
@@ -0,0 +1,10 @@
+namespace Hind
+{
+    public class FoodProduct
+    {
+        public string Name { get; set; }
+        public string Shop { get; set; }
+        public decimal Price { get; set; }
+        public System.DateTime DateUpdated { get; set; }
+    }
+}
